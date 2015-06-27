117f16915268780c487bb87d392e2d2144387bc1
Lenakeiz <andrea.castegnaro@outlook.com>
2015-06-27 15:40:38 +0100

    Added flickr fetch in account page

--- app/www/js/controllers.js
+++ app/www/js/controllers.js
@@ -49,6 +49,49 @@ angular.module('starter.controllers', [])
 
 })
 
-.controller('AccountCtrl', function($scope) {
+.controller('AccountCtrl', function($scope, $http) {
 
+    $scope.searchGeo= function(){
+        navigator.geolocation.getCurrentPosition(function(position) {
+
+            //console.log(position.coords.latitude);
+            var apikey = "0ce4e9dc51e53f48128403886f827dbe";
+            //var photosByLocation = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=" + apikey + "&sort=interestingness-desc&privacy_filter=1&media=photos&lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&radius=0.2&per_page=10&page=1&format=json&nojsoncallback=1&auth_token=72157654714260099-4315478173f74ab0&api_sig=f85fc7526f0c01fe6074c6495e0654cf";
+
+            var bkupUrl = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=31b5e4dd35373f2889a2b477ab491030&sort=interestingness-desc&privacy_filter=1&media=photos&lat=51.530881&lon=-0.152524&radius=0.2&per_page=10&page=1&format=json&nojsoncallback=1&auth_token=72157654714260099-4315478173f74ab0&api_sig=f85fc7526f0c01fe6074c6495e0654cf";
+            $scope.photos = {};
+            var numPhotos = 0;
+            $http.get(bkupUrl).
+                success(function(data, status, headers, config) {
+                    // this callback will be called asynchronously
+                    // when the response is available
+
+                    angular.forEach(data.photos.photo, function(photoelement){
+                        console.log(photoelement.id);
+
+                        var photoUrl = "https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=31b5e4dd35373f2889a2b477ab491030&photo_id=" + photoelement.id.toString() + "&format=json&nojsoncallback=1";
+
+                        $http.get(photoUrl).
+                            success(function(data, status, headers, config){
+                                console.log(data.sizes.size[5].source);
+                                $scope.photos[numPhotos] = data.sizes.size[5].source;
+                                numPhotos++;
+                            }).
+                            error(function(data, status, headers, config) {
+                                alert("Unable to download photo");
+                            });
+
+
+                    });
+                }).
+                error(function(data, status, headers, config) {
+                    // called asynchronously if an error occurs
+                    // or server returns response with an error status.
+                    alert("Unable to find photos");
+                });
+
+        });
+
+    }
 });
+
