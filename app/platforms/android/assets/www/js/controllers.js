f0a2537791d4d46f4a55205b2b8667282f00a20f
artfloriani <artfloriani@gmail.com>
2015-06-27 15:50:16 +0100

    + View images, fix side scroll

--- app/platforms/android/assets/www/js/controllers.js
+++ app/platforms/android/assets/www/js/controllers.js
@@ -1,17 +1,22 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function ($scope, pastPhotos, wsAPI) {
+.controller('DashCtrl', function ($scope, pastPhotos, wsAPI, $ionicModal) {
     $scope.images = {};
+    var photos = [];
+    photos = pastPhotos;
     $scope.images = pastPhotos;
 
-
-    navigator.geolocation.getCurrentPosition(function(position) {
-        console.log(position.coords.latitude);
-    });
-
-    $scope.searchPhotos = function (value) {
+    var photoSize = [302, 227, 177, 127, 127, 127, 177, 227, 302];
+    
 
 
+    for (var i = 0; i < photos.length; i++) {
+        var total = 0;
+        for (var j = 0; j < photos[i].src.length; j++) {
+            total += photoSize[j % photoSize.length] + 50;
+
+        }
+        $scope.images[i].width = total/2;
     }
 
     $scope.imgClass = {};
@@ -37,7 +42,42 @@ angular.module('starter.controllers', [])
 
     }
 
-    
+
+
+    $ionicModal.fromTemplateUrl('templates/photo.html', function($ionicModal) {
+        $scope.modal = $ionicModal;
+    }, {
+        // Use our scope for the scope of the modal to keep it simple
+        scope: $scope,
+        // The animation we want to use for the modal entrance
+        animation: "fade-in"
+    });  
+
+    $scope.loadPhoto = function (date, photo) {
+        $scope.currentPhoto = photo;
+        $scope.currentDate = date;
+
+        $scope.currentPhotoUrl = $scope.images[date].src[photo];
+        console.log($scope.currentPhotoUrl);
+
+        $scope.modal.scope = $scope;
+        $scope.modal.show();
+        
+    }
+
+    $scope.swipeLeft = function () {
+       
+        var date = $scope.currentDate;
+        var photo = ($scope.currentPhoto + 1) % $scope.images[date].src.length;
+        $scope.currentPhoto = photo;
+        $scope.currentPhotoUrl = $scope.images[date].src[photo];
+    }
+    $scope.swipeRight = function () {
+        var date = $scope.currentDate;
+        var photo = ($scope.currentPhoto - 1) % $scope.images[date].src.length;
+        $scope.currentPhoto = photo;
+        $scope.currentPhotoUrl = $scope.images[date].src[photo];
+    }
 
 })
 
